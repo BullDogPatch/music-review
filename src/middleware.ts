c6e4b734f6a65d7eb8662dcf51ea5375340bd48e
Craig Clayton <craig__clayton@outlook.com>
2025-05-05 15:41:22 +0100

    add homepage

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/user-profile(.*)']);
+const isProtectedRoute = createRouteMatcher([
+  '/user-profile(.*)',
+  '/create-post',
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
