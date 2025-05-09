09b01f4843f7b77feb3a56e190042253da4a492f
Craig Clayton <craig__clayton@outlook.com>
2025-05-09 15:53:51 +0100

    wrap api calls in try/catch

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,30 +1,43 @@
 import { db } from './dbConnection';
-import { Post } from './types';
 
 export const fetchUser = async (username: string) => {
-  const user = await db.query(
-    `SELECT * FROM music_app_users WHERE clerk_id = (SELECT clerk_id FROM music_app_users WHERE username = $1)`,
-    [username]
-  );
-  return user.rows[0];
+  try {
+    const user = await db.query(
+      `SELECT * FROM music_app_users WHERE clerk_id = (SELECT clerk_id FROM music_app_users WHERE username = $1)`,
+      [username]
+    );
+    return user.rows[0];
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    throw new Error('Failed to fetch user');
+  }
 };
 
 export const fetchAllPosts = async () => {
-  const { rows } = await db.query(`SELECT * FROM music_user_posts`);
-  return rows;
+  try {
+    const { rows } = await db.query(`SELECT * FROM music_user_posts`);
+    return rows;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    throw new Error('Failed to fetch posts');
+  }
 };
 
 export const fetchPostsByUsername = async (username: string) => {
-  const posts = await db.query(
-    `
-    SELECT up.id, up.title, up.description, up.image, up.created_at, u.username, u.clerk_id
-    FROM music_user_posts up
-    JOIN music_app_users u ON up.user_id = u.id
-    WHERE u.username = $1
-    ORDER BY up.created_at DESC
-    `,
-    [username]
-  );
-
-  return posts.rows;
+  try {
+    const posts = await db.query(
+      `
+      SELECT up.id, up.title, up.description, up.image, up.created_at, u.username, u.clerk_id
+      FROM music_user_posts up
+      JOIN music_app_users u ON up.user_id = u.id
+      WHERE u.username = $1
+      ORDER BY up.created_at DESC
+      `,
+      [username]
+    );
+    return posts.rows;
+  } catch (error) {
+    console.error(`Error fetching posts for ${username}:`, error);
+    throw new Error('Failed to fetch user posts');
+  }
 };
